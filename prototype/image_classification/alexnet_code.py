903a7b4ba517627cd851f98a8ca7c8024f3faf2f
somedamnauthor <sreeshnelp@gmail.com>
2023-04-24 17:56:45 +0200

    added wrapper code that wraps model into a flask app

--- prototype/image_classification/alexnet_code.py
+++ prototype/image_classification/alexnet_code.py
@@ -11,32 +11,53 @@ warnings.filterwarnings("ignore")
 
 # Input
 
-image_path = sys.argv[1]
-classes_path = sys.argv[2]
-with open(classes_path) as f:
-    classes = [line.strip() for line in f.readlines()]
-    
-transform = transforms.Compose([            #[1]
- transforms.Resize(256),                    #[2]
- transforms.CenterCrop(224),                #[3]
- transforms.ToTensor(),                     #[4]
- transforms.Normalize(                      #[5]
- mean=[0.485, 0.456, 0.406],                #[6]
- std=[0.229, 0.224, 0.225]                  #[7]
- )])
+class Model:
 
-img = Image.open(image_path)
-img_t = transform(img)
-batch_t = torch.unsqueeze(img_t, 0)
+    def model_definition(self):
+
+        alexnet = models.alexnet(pretrained=True)
+        alexnet.eval()
+        return alexnet
 
 
-# Model import and inference
+    def preprocess_input(self, image_path):
+     
+        transform = transforms.Compose([            #[1]
+         transforms.Resize(256),                    #[2]
+         transforms.CenterCrop(224),                #[3]
+         transforms.ToTensor(),                     #[4]
+         transforms.Normalize(                      #[5]
+         mean=[0.485, 0.456, 0.406],                #[6]
+         std=[0.229, 0.224, 0.225]                  #[7]
+         )])
 
-alexnet = models.alexnet(pretrained=True)
-alexnet.eval()
+        img = Image.open(image_path)
+        img_t = transform(img)
+        batch_t = torch.unsqueeze(img_t, 0)
 
-out = alexnet(batch_t)
-_, index = torch.max(out, 1)
-percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100
+        return batch_t
 
-print(classes[index[0].item()], percentage[index[0]].item())
+
+    def predict(self, model, batch_t, classes_path):
+
+        with open(classes_path) as f:
+            classes = [line.strip() for line in f.readlines()]
+
+        out = model(batch_t)
+        _, index = torch.max(out, 1)
+        percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100
+        # print(classes[index[0].item()], percentage[index[0]].item())
+
+        return classes[index[0].item()]
+
+
+# model = Model()
+
+# alexnet = model.model_definition()
+
+# image_path = sys.argv[1]
+# batch_t = model.preprocess_input(image_path)
+
+# classes_path = sys.argv[2]
+# prediction = model.predict(alexnet, batch_t, classes_path)
+# print(prediction)
---
+++ prototype/image_classification/wrapper.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+from flask_restful import reqparse, abort, Api, Resource
+from alexnet_code import Model
+import os
+import sys
+
+app = Flask(__name__)
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+
+	if request.method == 'POST':
+		data_path = request.form['data_path']
+		classes_path = request.form['classes_path']
+
+		model = Model()
+
+		model_definition = model.model_definition()
+		batch_t = model.preprocess_input(data_path)
+		prediction = model.predict(model_definition, batch_t, classes_path)
+
+		output = {'prediction':prediction}
+
+		return output
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
