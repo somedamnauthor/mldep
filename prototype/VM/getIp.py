e63006f0cf28028c07f60e19ad25f589a8af37a2
somedamnauthor <sreeshnelp@gmail.com>
2023-07-03 20:36:22 +0200

    moved location of package installation script and made necessary adjustment to vm deployment script

--- prototype/VM/getIp.py
+++ prototype/VM/getIp.py
@@ -23,16 +23,22 @@ for line in fstring:
   
 # displaying the extracted IP addresses
 try:
-    ip = lst[-1][:-3]
+    ip = lst[0][:-3]
 except:
     pass
 
+ip = "192.168.122.139"
+
 copy_command = "sshpass -p 'admin' scp -oStrictHostKeyChecking=no -r ../image_classification/ ubuntu@"+ip+":/home/ubuntu/ml/"
+copy_install_command = "sshpass -p 'admin' scp -oStrictHostKeyChecking=no -r installPackages.sh ubuntu@"+ip+":/home/ubuntu/ml/"
 install_command = "sshpass -p 'admin' ssh ubuntu@"+ip+" 'sh /home/ubuntu/ml/installPackages.sh'"
 start_command = "sshpass -p 'admin' ssh ubuntu@"+ip+" 'cd /home/ubuntu/ml; python3 wrapper.py'"
 
 print(copy_command)
 os.system(copy_command)
+
+print(copy_install_command)
+os.system(copy_install_command)
 
 print(install_command)
 os.system(install_command)
---
+++ prototype/temp/installPackages.sh
@@ -0,0 +1,17 @@
+sudo apt update -y
+
+sudo apt install python3-pip -y
+
+pip3 install flask
+
+#pip3 install torch --find-links https://download.pytorch.org/whl/torch_stable.html
+
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+
+sudo apt install make automake gcc g++ -y
+
+#pip3 install torchvision --find-links https://download.pytorch.org/whl/torch_stable.html
+
+pip3 install Pillow
+
+pip3 install flask_restful
