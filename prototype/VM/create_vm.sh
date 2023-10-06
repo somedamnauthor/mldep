2259c1daba4bed0af81f41341a7fc5198967c5e6
somedamnauthor <sreeshnelp@gmail.com>
2023-10-06 21:12:57 +0200

    removed console during installation from vm deployment

--- prototype/VM/create_vm.sh
+++ prototype/VM/create_vm.sh
@@ -6,6 +6,6 @@ qemu-img create -b base_images/focal.img -f qcow2 -F qcow2 mldepimg${1}.img 20G
 
 genisoimage -output cidata${1}.iso -V cidata -r -J user-data meta-data
 
-virt-install --name=mldep_vm${1} --ram=2048 --vcpus=1 --import --disk path=mldepimg${1}.img,format=qcow2 --disk path=cidata${1}.iso,device=cdrom --network bridge=virbr0,model=virtio --graphics none
+virt-install --name=mldep_vm${1} --ram=2048 --vcpus=1 --import --disk path=mldepimg${1}.img,format=qcow2 --disk path=cidata${1}.iso,device=cdrom --network bridge=virbr0,model=virtio --graphics none --noautoconsole
 
 virsh net-dhcp-leases default
